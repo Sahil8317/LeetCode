944abdc52df2b66ef642bc9140c4d3cd7573d011
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2021-12-27 01:43:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (26.41%) - LeetHub

---
+++ excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int p=0,k=26;
+        int n = columnTitle.size();
+        int res=0;
+        for(int i=0;i<n;i++){
+            p = (n-i)-1;
+            int temp = pow(26,p);
+            res+=temp*(columnTitle[i]-'@');
+        }
+        return res;
+    }
+};
