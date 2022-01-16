41f43ae9f5c5adad2f8b23209e3a8fea24ea4f87
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-01-16 16:01:25 +0530

    Time: 7 ms (74.89%), Space: 10.9 MB (60.23%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    void findCom(vector<int> &can,int target,vector<vector<int>> &ans,vector<int> &res,int index){
+        if(target==0){
+            ans.push_back(res);
+            return ;
+        }
+        for(int i=index;i<can.size();i++){
+            if(can[i]<=target){
+                res.push_back(can[i]);
+                findCom(can,target-can[i],ans,res,i);
+                res.pop_back();
+
+            }else{
+                return ;
+            }
+        }
+    }
+    
+    
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> res;
+        findCom(candidates,target,ans,res,0);
+        return ans;
+    }
+};
