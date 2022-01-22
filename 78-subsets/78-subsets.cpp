e8e288cfb2e286400354ca5f37a343adbbd46ac0
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-01-22 21:11:11 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (49.97%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    
-    void rFun(vector<int> &nums,vector<vector<int>> &ans,int index,vector<int> res){
-        ans.push_back(res);
-        for(int i=index;i<nums.size();i++){
-            res.push_back(nums[i]);
-            rFun(nums,ans,i+1,res);
-            res.pop_back();
-        }
-    }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        vector<int> res;
-        rFun(nums,ans,0,res);
+        int n = nums.size();
+        int numbers = (1<<n);   // 2 power n
+        for(int i=0;i<numbers;i++){
+            vector<int> res;
+            for(int j=0;j<nums.size();j++){
+                if((i & (1<<j))){
+                    res.push_back(nums[j]);
+                }
+            }
+            ans.push_back(res);
+        }
         return ans;
+        
     }
 };
