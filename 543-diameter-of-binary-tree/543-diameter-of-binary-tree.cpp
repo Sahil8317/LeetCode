778f6741c0276c0c93ae8b0bee6449a93a097a93
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-01-31 00:33:56 +0530

    Time: 8 ms (84.51%), Space: 20.3 MB (25.59%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,30 +11,34 @@
  */
 class Solution {
 public:
-    int calTree(TreeNode*p){
+    int calTree(TreeNode*p,int&maxi){
         if(!p)
             return 0;
-        int x = calTree(p->left)+1;
-        int y = calTree(p->right)+1;
-        if(x>y)
-            return x;
-        else
-            return y;
+        int x = calTree(p->left,maxi);
+        int y = calTree(p->right,maxi);
+        maxi = max(maxi,x+y);
+        return 1+max(x,y);
     }
     int diameterOfBinaryTree(TreeNode* root) {
+        
         if(root==NULL)
             return 0;
-        queue<TreeNode*>q;
-        q.push(root);
-        int ans = INT_MIN;
-        while(!q.empty()){
-            TreeNode * t = q.front();
-            q.pop();
-            if(t->left)q.push(t->left);
-            if(t->right)q.push(t->right);
-            int x = calTree(t->left)+calTree(t->right);
-            ans = max(ans,x);
-        }
+        int ans = 0;
+        calTree(root,ans);
+        
+        
+        // // brute force
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // int ans = INT_MIN;
+        // while(!q.empty()){
+        //     TreeNode * t = q.front();
+        //     q.pop();
+        //     if(t->left)q.push(t->left);
+        //     if(t->right)q.push(t->right);
+        //     int x = calTree(t->left)+calTree(t->right);
+        //     ans = max(ans,x);
+        // }
         return ans;
     }
 };
