ce064f6496e926c0e9c938a21360abf02dc1d69c
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-03-04 20:06:00 +0530

    Time: 263 ms (5.06%), Space: 44.6 MB (6.33%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
        map<pair<double,double>,double> dp;
-       for(int row= 0;row<=query_row;row++){ 
+        dp[{0,0}] = poured;
+       for(int row= 1;row<=query_row;row++){ 
            for(int glass = 0;glass<row+1;glass++){  // each row will have row+1 glasses
-               if(row==0 && glass==0){
-                   dp[{row,glass}] = poured;
-               }else{
                  double x = ((dp[{row-1,glass-1}]-1)/2);
                  double y = ((dp[{row-1,glass}]-1)/2);
                  if(x<0) x = 0;
                  if(y<0) y = 0;
-               dp[{row,glass}] = x+y;  //pascal traingle 
-               }   
-               if(row==query_row && glass==query_glass){ 
-                   if(dp[{row,glass}]>1) return 1.0;
-                   return dp[{row,glass}];
-               }
+               dp[{row,glass}] = x+y;  //pascal traingle   
+               if(row==query_row && glass==query_glass)
+                   return min(dp[{row,glass}],1.0);
+               
              }
          }
-        return 0.0;
+        return min(1.0,dp[{0,0}]);
     }
 };
