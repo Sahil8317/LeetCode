9dde17a01db5933a3464b611b21615cb7cf2b7c4
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-03-14 17:05:20 +0530

    Time: 4 ms (88.94%), Space: 8 MB (76.87%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> stk;
+        int i = 0;
+        int s = path.length();
+        while(i<path.length()){
+            if(path[i]=='/'){
+                while(i<s && path[i]=='/'){
+                    i++;
+                }
+                string d = "";
+                while(i<s && path[i]!='/'){
+                    d+=path[i];
+                    i++;
+                }
+                if(d=="."){
+                    d = "";
+                }else if(d==".."){
+                    if(!stk.empty()){
+                        stk.pop();
+                    }
+                }else if(d=="..."){
+                    stk.push(d);
+                }else if(d!=""){
+                    stk.push(d);
+                }
+            }
+        }
+        string ans = "/";
+        int n = stk.size();
+        string res = "/";
+        vector<string> ar;
+        
+        while(!stk.empty()){
+            ar.push_back(stk.top());
+            stk.pop();
+        }
+        reverse(ar.begin(),ar.end());
+        // for(int i=0;i<res.length();i++){
+        //     ans+=res[i];
+        //     if(i<res.length()-1)
+        //         ans+='/';
+        // }
+       // cout<< ar.size();
+        for(int i=0;i<ar.size();i++){
+             res+=ar[i];
+            if(i<ar.size()-1) res+='/';
+        }
+        return res;
+    }
+    
+};
