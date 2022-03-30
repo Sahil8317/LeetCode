63a5bed31b09467f3f4853a30b78a50683b62c7d
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-03-30 18:06:40 +0530

    Time: 3 ms (52.69%), Space: 7.9 MB (6.85%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -14,6 +14,14 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,-1);
-        return f(n-1,nums,dp);
+       // return f(n-1,nums,dp);
+        dp[0] = nums[0];
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take+=dp[i-2];
+            int notTake = 0+dp[i-1];
+            dp[i] = max(take,notTake);
+        }
+        return dp[n-1];
     }
 };
