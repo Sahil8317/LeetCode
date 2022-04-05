7bd3bda6c24dd9dcea279c906fc0fc0d4e8f457d
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-04-06 01:45:35 +0530

    Time: 7 ms (29.19%), Space: 7.9 MB (11.80%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -3,15 +3,15 @@
 class Solution {
 public:
     // memorization dp solution 
-    int f(int i,int j,vector<vector<int>> &mat,vector<vector<int>> &dp){
-        if(i>=0 && j>=0 && mat[i][j]==1) return 0;   // means there is an obstacle cannot go a head
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up = f(i-1,j,mat,dp);
-        int left = f(i,j-1,mat,dp);
-        return dp[i][j] = left+up;
-    }
+    // int f(int i,int j,vector<vector<int>> &mat,vector<vector<int>> &dp){
+    //     if(i>=0 && j>=0 && mat[i][j]==1) return 0;   // means there is an obstacle cannot go a head
+    //     if(i==0 && j==0) return 1;
+    //     if(i<0 || j<0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int up = f(i-1,j,mat,dp);
+    //     int left = f(i,j-1,mat,dp);
+    //     return dp[i][j] = left+up;
+    // }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
