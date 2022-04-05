810ceacf7d1481aa5e6760bd7bcb08979afc4217
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-04-06 01:45:12 +0530

    Time: 4 ms (65.64%), Space: 7.8 MB (57.93%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -2,7 +2,7 @@
 
 class Solution {
 public:
-    
+    // memorization dp solution 
     int f(int i,int j,vector<vector<int>> &mat,vector<vector<int>> &dp){
         if(i>=0 && j>=0 && mat[i][j]==1) return 0;   // means there is an obstacle cannot go a head
         if(i==0 && j==0) return 1;
@@ -16,6 +16,19 @@ public:
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,obstacleGrid,dp);
+        //return f(n-1,m-1,obstacleGrid,dp);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(obstacleGrid[i][j]==1) dp[i][j] = 0;
+                else if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                    int left = 0,up = 0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = left+up;
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
