00fb03a44a6261948aba911acef5041c812c0e5a
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-04-06 01:40:19 +0530

    Time: 3 ms (80.40%), Space: 7.9 MB (39.09%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -0,0 +1,21 @@
+// 2 D dp problem
+
+class Solution {
+public:
+    
+    int f(int i,int j,vector<vector<int>> &mat,vector<vector<int>> &dp){
+        if(i>=0 && j>=0 && mat[i][j]==1) return 0;   // means there is an obstacle cannot go a head
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = f(i-1,j,mat,dp);
+        int left = f(i,j-1,mat,dp);
+        return dp[i][j] = left+up;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,obstacleGrid,dp);
+    }
+};
