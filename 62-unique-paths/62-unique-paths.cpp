76952e12f942358044666391d7423284158ab365
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-04-05 17:36:36 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (43.88%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
+    // memorization dp
     int funct(int a,int b,int m,int n,vector<vector<int>>&dp){
         if(a==m-1 && b==n-1) return 1;
-        if(a>m-1)return 0;
-        if(b>n-1)return 0;
+        if(a>m-1 || b>n-1)return 0;
         if(dp[a][b]!=-1) return dp[a][b];
         dp[a][b] = funct(a+1,b,m,n,dp)+funct(a,b+1,m,n,dp);
         return dp[a][b];
     }
     
     int uniquePaths(int m, int n) {
-         vector<vector<int>> dp;
+         vector<vector<int>> dp(m,vector<int>(n,0));
+        //return funct(0,0,m,n,dp);
         for(int i=0;i<m;i++){
-            vector<int> v;
             for(int j=0;j<n;j++){
-              v.push_back(-1);
+                if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                    int left = 0,right = 0;
+                    if(i>0) left =dp[i-1][j];
+                    if(j>0) right = dp[i][j-1];
+                    dp[i][j] = left+right;
+                }
             }
-            dp.push_back(v);
         }
-       
-        return funct(0,0,m,n,dp);
+        return dp[m-1][n-1];
     }
 };
