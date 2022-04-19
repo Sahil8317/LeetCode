dee1a6fa39d360b964249dacebab0ccee47f5098
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-04-20 01:30:40 +0530

    Time: 14 ms (42.88%), Space: 10.1 MB (27.44%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -14,7 +14,23 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,grid,dp);
+       // return f(n-1,m-1,grid,dp);
         
+        // tabulation
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0) dp[i][j] = grid[i][j];
+                else{
+                    int right = grid[i][j];
+                    if(j>0) right+=dp[i][j-1];
+                    else right = 1e9;
+                    int down = grid[i][j];
+                    if(i>0) down+=dp[i-1][j];
+                    else down = 1e9;
+                    dp[i][j] = min(right,down);
+                }
+            } 
+        }
+        return dp[n-1][m-1];  
     }
 };
