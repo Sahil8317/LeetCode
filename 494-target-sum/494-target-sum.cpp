32feadcedf71805c8a44042fa05abb2d0681b77c
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-05-08 14:08:39 +0530

    Time: 361 ms (33.67%), Space: 54.8 MB (17.99%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     
-    int f(int index,vector<int> &nums,int target){
+    int f(int index,vector<int> &nums,int target,map<pair<int,int>,int> &dp){
         if(index==0){
             return (target+nums[index]==0)+(target-nums[index]==0);  // in some of the test case taking the index element in both the cases will give you the answer.
         }
-        int notTake = f(index-1,nums,target+nums[index]);
-        int take = f(index-1,nums,target-nums[index]);
+        if(dp.find({index,target})!=dp.end()) return dp[{index,target}];
+        int notTake = f(index-1,nums,target+nums[index],dp);
+        int take = f(index-1,nums,target-nums[index],dp);
         
-        return notTake+take;
+        return dp[{index,target}] = notTake+take;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(nums.size()-1,nums,target);
+        map<pair<int,int>,int> dp;
+        return f(nums.size()-1,nums,target,dp);
     }
 };
