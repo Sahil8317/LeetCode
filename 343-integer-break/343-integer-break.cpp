d7c51d0121f9cd3ea00a4574367bd300ed9db073
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-07-24 20:03:21 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (5.75%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    // memorization
     int f(int index,int n,vector<int> &arr,vector<vector<int>> &dp){
         if(index==0){
             return 1;
@@ -20,5 +20,21 @@ public:
             arr.push_back(i);
         
         return f(n-2,n,arr,dp);
+        //Tabulation
+        //base case
+        
+        // for(int N = 0;N<=n;N++){
+        //     dp[0][N] = 1;
+        // }
+        // for(int index = 1;index<n;index++){
+        //     for(int N = 0;N<=n;N++){
+        //         int notTake = (dp[index-1][N])*1;
+        //         int take =INT_MIN;
+        //         if(arr[index]<=N)
+        //             take = (dp[index][N-arr[index]])*arr[index];
+        //         dp[index][N] = max(take,notTake);
+        //     }
+        // }
+        // return dp[n-1][n];
     }
 };
