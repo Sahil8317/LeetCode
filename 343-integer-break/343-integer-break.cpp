d3aa3b3a3ec03ad4b1c5c7a95ffc8195303f505e
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-07-24 19:34:52 +0530

    Time: 4 ms (31.53%), Space: 6.8 MB (5.75%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     
-    int f(int index,int n,vector<int> &arr){
+    int f(int index,int n,vector<int> &arr,vector<vector<int>> &dp){
         if(index==0){
             return 1;
         }
-        int notTake = 1*f(index-1,n,arr);
+        if(dp[index][n]!=-1) return dp[index][n];
+        int notTake = 1*f(index-1,n,arr,dp);
         int take = INT_MIN;
         if(arr[index]<=n){
-            take = f(index,n-arr[index],arr)*arr[index];
+            take = f(index,n-arr[index],arr,dp)*arr[index];
         }
-        return max(take,notTake);
+        return dp[index][n] = max(take,notTake);
     }
     int integerBreak(int n) {
         vector<int> arr;
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
         for(int i=1;i<n;i++)
             arr.push_back(i);
         
-        return f(n-2,n,arr);
+        return f(n-2,n,arr,dp);
     }
 };
