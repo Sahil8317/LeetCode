76bd6dc479696ced57c0278c4a6fb4dddcbd85a8
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-08-01 23:26:36 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -12,6 +12,8 @@ public:
     int uniquePaths(int m, int n) {
          vector<vector<int>> dp(m,vector<int>(n,0));
         //return funct(0,0,m,n,dp);
+        // tabulation solution
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0 && j==0) dp[i][j] = 1;
