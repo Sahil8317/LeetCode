dbaa329ddcfde049f10b7afcedcb8adaafe3a60f
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-08-25 22:20:59 +0530

    Time: 50 ms (14.79%), Space: 9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char,int> str1,str2;
-        for(char c:ransomNote){
+        map<char,int> str1;
+        for(char c:magazine){
             str1[c]+=1;
         }
-        for(char c:magazine){
-            str2[c]+=1;
-        }
-       for(auto m:str1){
-          if (str2[m.first]<m.second)
-              return false;
+       for(char c:ransomNote){
+           if(str1[c]==0) return false;
+          str1[c]--;
        }
     return true;
     }
