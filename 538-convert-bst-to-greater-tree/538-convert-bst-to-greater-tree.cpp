9d2b6ef5cbc35606579493af5c5d979ba8efd7b8
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-09-10 16:32:29 +0530

    Time: 75 ms (26.37%), Space: 33.4 MB (62.24%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -11,40 +11,53 @@
  */
 class Solution {
 public:
-    
-    int sumOfNodes(TreeNode *node,int root){
-        if(node->left==NULL && node->right==NULL && root!=0){
-            int temp = node->val;
-            node->val+=root;
-            return temp;
-        }
-        if(!node->left && !node->right) return node->val;
-        if(root!=0){
-            // means we are on left side of tree
-            int temp= node->val;
-            node->val+=root;
-            if(node->right){
-                int k=sumOfNodes(node->right,root);
-                temp+=k;
-                node->val+=k;
-            }
-            int n = 0;
-            if(node->left) 
-                n =  sumOfNodes(node->left,node->val);
-            return temp+n;
-        }
-        if(node->right)
-            node->val+=sumOfNodes(node->right,0);
-        int ans = 0;
-        if(node->left)
-             ans = sumOfNodes(node->left,node->val);
-        return node->val+ans;
+    //initial approach
+//     int sumOfNodes(TreeNode *node,int root){
+//         if(node->left==NULL && node->right==NULL && root!=0){
+//             int temp = node->val;
+//             node->val+=root;
+//             return temp;
+//         }
+//         if(!node->left && !node->right) return node->val;
+//         if(root!=0){
+//             // means we are on left side of tree
+//             int temp= node->val;
+//             node->val+=root;
+//             if(node->right){
+//                 int k=sumOfNodes(node->right,root);
+//                 temp+=k;
+//                 node->val+=k;
+//             }
+//             int n = 0;
+//             if(node->left) 
+//                 n =  sumOfNodes(node->left,node->val);
+//             return temp+n;
+//         }
+//         if(node->right)
+//             node->val+=sumOfNodes(node->right,0);
+//         int ans = 0;
+//         if(node->left)
+//              ans = sumOfNodes(node->left,node->val);
+//         return node->val+ans;
             
+//     }
+    
+    // best solution
+    // inorder traversal with carrying a variable total 
+    //  right root left 
+    void travelNodes(TreeNode *node,int &total){
+        if(!node) return;
+        travelNodes(node->right,total);
+        total+=node->val;
+        node->val = total;
+        travelNodes(node->left,total);
+        
     }
     
     TreeNode* convertBST(TreeNode* root) {
+        int total = 0;
         if(root)
-            sumOfNodes(root,0);
+            travelNodes(root,total);
         return root;
     }
 };
