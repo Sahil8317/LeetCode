6dd8fd034a52d2226c6e9766707a4ee88dbe9e40
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-09-16 01:32:06 +0530

    Time: 1169 ms (13.58%), Space: 10.7 MB (21.50%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    
+    // one-d dp is enough as we only need to store the prev result 
+    // in 2-d dp we do dp[index][prev+1] so all the time we were on the current index only.
+    // better to remove as we dont need it.
     int fun(int index,int prev,vector<int> &nums,vector<int>&dp){
         if(index==nums.size())
             return 0;
@@ -21,11 +23,17 @@ public:
         vector<int> dp(n+1,-1);
         ans = fun(0,-1,nums,dp);
         return ans;
-//         for(int prev=0;prev<=n;prev++){
-//             dp[n][prev] = 0;
-//         }
-        
-       
-        
+        //  vector<vector<int>> dp2(n+1,vector<int>(n+1,0));
+        //  // base case
+        // for(int index = n-1;index>=0;index--){
+        //     for(int prev = n-1;prev>=-1;prev--){
+        //          int take = 0;
+        //         if( prev==-1 || nums[index]>nums[prev] )
+        //            take = 1+dp2[index+1][index];
+        //         int notTake =0+ dp2[index+1][prev];
+        //          dp2[index][prev+1] =  max(take,notTake);
+        //     }
+        // }
+        // return dp2[0][0];
     }
 };
