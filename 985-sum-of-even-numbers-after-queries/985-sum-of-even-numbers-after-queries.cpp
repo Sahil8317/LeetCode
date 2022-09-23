37c717dce2f4d2c53c834e7395456e8d7df7cc23
Sahil8317 <61882338+Sahil8317@users.noreply.github.com>
2022-09-24 01:32:45 +0530

    Time: 214 ms (70.10%), Space: 49.9 MB (19.34%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -3,34 +3,28 @@ public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
         vector<int> ans;
         int n = nums.size();
-        map<int,int> m;
         int sum =0;
         for(int i=0;i<n;i++){
-            if(nums[i]%2==0){
-                m[i] = nums[i];
+            if(nums[i]%2==0)
                 sum+=nums[i];
-            }
+            
         }
+         
+        //1. if prev nums[index] is even then remove it after also its even then again add it
+        //2. if prev nums[index] is even and after it becomes odd then you already removed it
+        //3. if prev nums[index] is odd then it becomes even then just add that to sum
+        //4. if prev nums[index] is odd and after also its odd only then no effect on sum  
         for(auto vec:queries){
-           int val = vec[0];
-           int index = vec[1];
-           nums[index] = nums[index]+val;
-           if(nums[index]%2==0){
-               if(m.find(index)!=m.end()){
-                   // means that number already exits before now you have to take its diff.
-                   int diff = nums[index]- m[index];
-                   sum+=diff;
-               }else{
-                   sum+=nums[index];
-               }
-                m[index] = nums[index];
-           }else{
-               if(m.find(index)!=m.end()){
-                  sum = sum-m[index];
-                   m.erase(index) ;
-               }
-           }
-             ans.push_back(sum);
+          int index = vec[1];
+          int val = vec[0];
+          if(nums[index]%2==0){
+              sum -=nums[index];
+          }
+          nums[index]+=val;
+          if(nums[index]%2==0){
+              sum+=nums[index];
+          }
+            ans.push_back(sum);
         }
         if(ans.empty()) ans.push_back(0);
         return ans ;
